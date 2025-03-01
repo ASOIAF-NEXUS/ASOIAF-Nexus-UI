3078e9b9077180a4eedd4465e9a04ee52b56d545
Samuel Turgeon <sam_837@hotmail.com>
2025-02-28 21:00:06 -0500

    Actually get Mantine to show the AppShell as it should be

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { AppShell, Burger, Flex, Skeleton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [opened, { toggle }] = useDisclosure(true);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AppShell
+      header={{ height: "8em" }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened, desktop: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header px="md" py="sm">
+        <Flex justify={"flex-start"}>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+        ></Burger>
+          </Flex>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+        </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Footer>Footer</AppShell.Footer>
+    </AppShell>
+  );
 }
 
-export default App
+export default App;
